6669f9bd066824951eb430f0539d7eb3630db2ca
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2026-04-06 17:56:00 -0400

    Remove: binary-search - submission-0

--- Data Structures & Algorithms/binary-search/submission-0.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int search(int[] nums, int target) {
-       int index = Arrays.binarySearch(nums, target);
-        return index >= 0 ? index : -1;
-    }
-}
-
-//Time Complexity: O(log n)
